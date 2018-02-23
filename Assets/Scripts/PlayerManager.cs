eb7af26a2173db553387ac2202e12cc3248159ea
yibojiang <yibojiang00@gmail.com>
2018-02-22 22:48:19 -0500

    import google vr

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(player.transform.position);
+		Debug.Log(player.transform.TransformDirection(Vector3.forward));
+		if (Input.GetKeyDown(KeyCode.F)){
+			Debug.Log("shoot");
+		}
+	}
+}
